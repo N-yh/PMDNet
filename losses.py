0b7ea2b571878df9409a23e5e846a2c275dd50cb
N-yh <2042601874@qq.com>
2026-02-04 17:38:58 +0800

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models.vgg import vgg16
+# from torchvision.models.vgg import vgg16
 
 
 ##########################################################################
